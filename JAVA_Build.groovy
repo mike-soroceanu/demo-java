310d20fb4945a4227e704928ed41bcb257671ee7
KingMike1998 <mihaisoroceanu6@gmail.com>
2022-09-10 13:17:56 +0300

    Modified name of one groovy and added a Deploy

---
+++ JAVA_Build.groovy
@@ -0,0 +1,68 @@
+pipeline {
+  agent {label 'slave01'}
+
+  tools {
+    maven 'Maven 3.6'
+    jdk "JDK 1.8"
+  }
+
+
+	environment {
+		DOCKERHUB_CREDENTIALS=credentials('DockerHub')
+	}
+
+  stages{
+    stage ('Initialize') {
+      steps{
+        sh '''
+          echo "PATH = ${PATH}"
+          echo "M2_HOME = ${M2_HOME}"
+        '''
+      }
+    }
+
+    stage ('Build maven application') {
+      steps {
+        sh '''
+          mvn package
+        '''
+      }
+    }
+
+    stage ('Store the artifact') {
+      steps{
+        sh '''
+          mkdir -p pkg
+          mv target/demo.war pkg/demo.war
+        '''
+      }
+    }
+
+    stage ('Build Docker image') {
+      steps { 
+        script { 
+         sh "docker build -t mikesoroceanu/simplejavaapp-mvn-docker:${BUILD_ID} ."
+         sh "docker build -t mikesoroceanu/simplejavaapp-mvn-docker:latest ."
+        }
+      }
+    }
+
+
+		stage('Login') {
+
+			steps {
+				sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+			}
+		}
+
+
+		stage('Push') {
+
+			steps {
+				sh "docker push mikesoroceanu/simplejavaapp-mvn-docker:${BUILD_ID}"
+        sh "docker push mikesoroceanu/simplejavaapp-mvn-docker:latest"
+			}
+		}
+	
+  }
+}
--- JAVA_Deploy.groovy
+++ JAVA_Deploy.groovy
@@ -12,57 +12,26 @@ pipeline {
 	}
 
   stages{
-    stage ('Initialize') {
-      steps{
-        sh '''
-          echo "PATH = ${PATH}"
-          echo "M2_HOME = ${M2_HOME}"
-        '''
-      }
-    }
-
-    stage ('Build maven application') {
-      steps {
-        sh '''
-          mvn package
-        '''
-      }
-    }
-
-    stage ('Store the artifact') {
-      steps{
-        sh '''
-          mkdir -p pkg
-          mv target/demo.war pkg/demo.war
-        '''
-      }
-    }
-
-    stage ('Build Docker image') {
-      steps { 
-        script { 
-         sh "docker build -t mikesoroceanu/simplejavaapp-mvn-docker:${BUILD_ID} ."
-         sh "docker build -t mikesoroceanu/simplejavaapp-mvn-docker:latest ."
-        }
-      }
-    }
-
-
 		stage('Login') {
 
 			steps {
-				sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
-			}
+				sh "echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin"
 		}
 
 
 		stage('Push') {
 
 			steps {
-				sh "docker push mikesoroceanu/simplejavaapp-mvn-docker:${BUILD_ID}"
-        sh "docker push mikesoroceanu/simplejavaapp-mvn-docker:latest"
+				sh "docker pull mikesoroceanu/simplejavaapp-mvn-docker:latest"
 			}
 		}
+
+        stage('Run container') {
+
+            steps{
+                sh "docker run --name MySimpleJavaApp-${BUILD_ID} --rm -d -p 5000:8080 mikesoroceanu/simplejavaapp-mvn-docker:latest"
+            }
+        }
 	
   }
 }
